91fdedfb12a77786a78b7b0877b7ec9f8a54d733
Christopher Phan <cphan@chrisphan.com>
2023-03-28 11:17:29 -0500

    Add copyright header to each rust file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+/* This file is part of Combodate: <https://github.com/christopherphan/combodate_rust>
+ *
+ * Copyright 2023 Christopher Phan <cphan@chrisphan.com>
+ *
+ * Available under an MIT license. See LICENSE.TXT in repository root directory for more
+ * information.
+ */
+
 use chrono::{DateTime, Datelike, Local, SecondsFormat, TimeZone, Timelike, Utc};
 
 use std::fmt::Display;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/* This file is part of Combodate: <https://github.com/christopherphan/combodate_rust>
+ *
+ * Copyright 2023 Christopher Phan <cphan@chrisphan.com>
+ *
+ * Available under an MIT license. See LICENSE.TXT in repository root directory for more
+ * information.
+ */
 fn main() {
     combodate::run();
 }
